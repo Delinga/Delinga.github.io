3aa163fccb5a8083c75e6316aaf09d660aa7f279
Delinga <JohnS.Pam@web.de>
2022-07-12 15:40:28 +0200

    Basic

---
+++ Projects/Sudoku_Solver/sudoku_script.js
@@ -0,0 +1,18 @@
+let sudokuArray = [];
+let solverID = 0;
+
+function getPossibleNumbers(sArray = [], possibleNumbers = {}){
+    
+    return possibleNumbers;
+}
+
+function checkIfSolved(sArray = []){
+
+    return;
+}
+
+function Solver(sArray = []){
+    if (!checkIfSolved){
+
+    }
+}
